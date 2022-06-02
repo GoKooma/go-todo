61f42e640b594c9a6dd3eb8a15ec121b753e2278
KweonMo Koo <mikekoo2010@gmail.com>
2022-06-02 01:38:02 -0400

    Clean up

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,10 +1,6 @@
 const tailwindcss = require('tailwindcss');
 
 module.exports = {
-  // plugins: {
-  //   tailwindcss: {},
-  //   autoprefixer: {},
-  // },
   plugins: [
     tailwindcss('./tailwind.config.js'),
     require('autoprefixer')
