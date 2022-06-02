21cbf6171b69f4e70c2a5093726a93d25f59e3c8
KweonMo Koo <mikekoo2010@gmail.com>
2022-06-02 01:36:35 -0400

    Add Chakra UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { ChakraProvider, Link, Text } from '@chakra-ui/react';
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import SignIn from "./components/SignIn";
 // import Notes from "./components/Notes";
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} exact />
-      <Route path="/sign-in" element={<SignIn />} exact />
-      {/* <Route path="/notes" element={Notes} exact /> */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
+  <ChakraProvider>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <Text fontSize='2xl'>
+              Home
+            </Text>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/sign-in" element={<SignIn />} exact />
+        {/* <Route path="/notes" element={Notes} exact /> */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </ChakraProvider>
 );
 
 export default App;
